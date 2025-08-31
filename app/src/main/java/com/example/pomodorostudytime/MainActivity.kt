7cdda20254ed7d8d76c03443e25e3703cde7cff0
fhaddad9 <ferashaddadgit@gmail.com>
2025-09-01 00:28:49 +0100

    variables and listeners implemented

--- app/src/main/java/com/example/pomodorostudytime/MainActivity.kt
+++ app/src/main/java/com/example/pomodorostudytime/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.pomodorostudytime
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // UI elements in use for timer
+    private lateinit var startButton: Button
+    private lateinit var pauseButton: Button
+    private lateinit var resetButton: Button
+
+    private lateinit var timerTextView: TextView
+
+    // Timer elements and values
+    private var countDownTimer: CountDownTimer? = null
+    private var isTimerRunning = false
+    private var timeLeftInMilis: Long = 25 * 60 * 1000
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        timerTextView = findViewById(R.id.timerTextView)
+        startButton = findViewById(R.id.startButton)
+        pauseButton = findViewById(R.id.pauseButton)
+        resetButton = findViewById(R.id.resetButton)
+
+        startButton.setOnClickListener {
+            startTimer()
+        }
+        pauseButton.setOnClickListener {
+            pauseTimer()
+        }
+        resetButton.setOnClickListener {
+            resetTimer()
         }
     }
 }
